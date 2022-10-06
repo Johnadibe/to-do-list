56010de2691e3a627db0029788b31aeb8d357e65
Johnadibe <40009354+Johnadibe@users.noreply.github.com>
2022-10-06 12:16:50 +0100

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './index.css';
-import { updateTodo } from './updateTodo.js'
+import updateTodo from './updateTodo.js';
 
 const formInput = document.querySelector('#form-input');
 const todoLists = document.querySelector('.to-do-lists');
@@ -7,11 +7,11 @@ const todoInput = document.querySelector('.to-do-input');
 
 let todos = JSON.parse(localStorage.getItem('todos'));
 const displayTodo = () => {
-    let li = '';
-    if (todos) {
-        todos.forEach((todo) => {
-            const completed = todo.completed ? 'checked' : '';
-            li += `<li class="list-item">
+  let li = '';
+  if (todos) {
+    todos.forEach((todo) => {
+      const completed = todo.completed ? 'checked' : '';
+      li += `<li class="list-item">
                         <label for="">
                             <input type="checkbox" ${completed} class="checkbox" data-check="${todo.index}">
                             <input type="text" class="todo-description" data-desc="${todo.index}" value="${todo.description}">
@@ -20,50 +20,50 @@ const displayTodo = () => {
                             <button class="trash-btn" id="${todo.index}">&#935;</button>
                         </div>
                     </li>`;
-        });
-    }
-    todoLists.innerHTML = li;
+    });
+  }
+  todoLists.innerHTML = li;
 };
 displayTodo();
 
 const addTodo = (e) => {
-    e.preventDefault();
-    const userInput = todoInput.value.trim();
-    todoInput.value = '';
-    if (!userInput) return;
-    if (!todos) {
-        todos = [];
-    }
-    const list = {
-        description: userInput,
-        completed: false,
-        index: todos.length,
-    };
-    todos.push(list);
-    localStorage.setItem('todos', JSON.stringify(todos));
-    displayTodo();
-}
+  e.preventDefault();
+  const userInput = todoInput.value.trim();
+  todoInput.value = '';
+  if (!userInput) return;
+  if (!todos) {
+    todos = [];
+  }
+  const list = {
+    description: userInput,
+    completed: false,
+    index: todos.length,
+  };
+  todos.push(list);
+  localStorage.setItem('todos', JSON.stringify(todos));
+  displayTodo();
+};
 
 formInput.addEventListener('submit', addTodo);
 
 const deleteTodo = (index) => {
-    const newTodo = todos.filter((element) => element.index !== index);
-    todos.length = 0;
-    let i = 0;
-    newTodo.forEach((element) => {
-        element.index = i;
-        i = i + 1;
-    });
-    todos.push(...newTodo);
-    localStorage.setItem('todos', JSON.stringify(todos));
-    displayTodo();
+  const newTodo = todos.filter((element) => element.index !== index);
+  todos.length = 0;
+  let i = 0;
+  newTodo.forEach((element) => {
+    element.index = i;
+    i += 1;
+  });
+  todos.push(...newTodo);
+  localStorage.setItem('todos', JSON.stringify(todos));
+  displayTodo();
 };
 
 todoLists.addEventListener('click', (e) => {
-    if (e.target.classList.contains('trash-btn')) {
-        const index = parseInt(e.target.getAttribute('id'), 10);
-        deleteTodo(index);
-    }
+  if (e.target.classList.contains('trash-btn')) {
+    const index = parseInt(e.target.getAttribute('id'), 10);
+    deleteTodo(index);
+  }
 });
 
 todoLists.addEventListener('click', updateTodo);
